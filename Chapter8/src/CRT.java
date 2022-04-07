abc24a039ff6efd187691b4e6d257c05e7994f6b
904101002 <904101002@L815WD220028.edu.cbe.ab.ca>
2022-04-07 09:28:37 -0600

    hi

---
+++ Chapter8/src/CRT.java
@@ -0,0 +1,31 @@
+/*
+
+Program: CRT.java         Last Date of this Revision: Apr 7, 2022
+
+Purpose: 
+
+Author: Said Rahmani, 
+School: CHHS
+Course: Computer Programming 30
+ 
+
+*/
+
+/* Screen Dump
+ 
+1. is-a is an inheritance of classes has-a is qualities the parent doesn't have.
+2. stop() and go()
+3. Abstract methods have no body, overriding a method is when the body and or parameters of a method is changed
+4. Abstracts class needs to be inherited. An interface class cannot be inherited 
+6.
+Abstract method
+Interface name
+doThat() is an abstract method so it needs to be inherited
+doThis(), doThat(), and doNow().
+doThis() in Roo object overwrites the Bo objects doThis() 
+f)  the parent class receives the 1 and initializes the properties for the object
+g) Since Roo extends to Bo it has access to the accessor methods in Bo, therefore a Roo object would be able to call the doThis() method.
+h) A method in Roo can call the the doThis() method using super
+
+ 
+ */
