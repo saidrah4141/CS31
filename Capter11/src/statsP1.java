0a1dccaaad107b8aa57cab934a0d9c5764b234b3
904101002 <904101002@L815WD220028.edu.cbe.ab.ca>
2022-04-12 10:11:53 -0600

    hi

--- Capter11/src/statsP1.java
+++ Capter11/src/statsP1.java
@@ -13,18 +13,47 @@ Course: Computer Programming 30
 
 
 import java.io.*;
+import java.util.Scanner;
 public class statsP1 {
 
 	public static void main(String[] args) 
 	
 	{
-		File dataFile= new File("scores. dat");
-		FileReader in;
-		BufferedReader readFile;
-		String score;
+		
 		double avgScore;
 		double totalScores = 0;
 		int numScores = 0;
+		
+		try {
+		      File dataFile = new File("test1.dat");
+		      Scanner myReader = new Scanner(dataFile);
+		      while (myReader.hasNextLine()) {
+		        String score = myReader.nextLine();
+		        System.out.println(score);
+		       totalScores += Double.parseDouble(score);
+		       numScores+=1;
+		      }
+		     avgScore= totalScores/numScores;
+		     System.out.println(avgScore);
+			
+		      
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+			}
+}
+		
+		
+		
+		/*
+		
+		File dataFile= new File("test1.dat");
+		FileReader in;
+		BufferedReader readFile;
+	
 		
 		try {
 	
@@ -33,10 +62,10 @@ public class statsP1 {
 		while ((score= readFile.readLine ()) != null ) {
 		numScores += 1;
 		System.out.println(score);
-		totalScores += Double.parseDouble (score);
+		totalScores += Double.parseDouble(score);
 		}
 		avgScore = totalScores / numScores;
-		System. out.println("Average =" + avgScore);
+		System.out.println("Average =" + avgScore);
 		readFile.close();
 		in.close();
 		}
@@ -45,18 +74,17 @@ public class statsP1 {
 		System. out. println("File does not exist or could not be found.");
 		System.err.println("FileNotFoundException:" + e. getMessage ());
 		}
-			catch (I0Exception e)
+		catch (I0Exception e)
 		{
 				System.out.println("Problem reading file.");
 				System.err.println("I0Exception: "+ e.getMessage());
 		
 		}
-		
+		*/
 		
 
 
-	}
-}
+
 
 
 
