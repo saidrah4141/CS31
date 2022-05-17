5e0e750d0e01d15ad9bc1363f85cb57afed781a7
904101002 <904101002@L815WD220028.edu.cbe.ab.ca>
2022-05-17 09:26:51 -0600

    hi

---
+++ Capter11/src/CRT.java
@@ -0,0 +1,25 @@
+/*
+
+1. No since files and memory are stored and used differently
+
+2. import java.io.File;
+import java.io.IOException;
+
+3.  \\ instead of  \
+
+4. 
+try
+{
+textFile.createNewFile();
+output.setText("File has been created.");
+}
+catch (IOException e)
+{
+output.setText("File could not be created.");
+System.err.println("IOException: " + e.getMessage());
+}
+
+5. A err stream
+	B on the screen
+
+*/
